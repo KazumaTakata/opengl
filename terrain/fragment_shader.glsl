4fa24149dcb4e3b12331bef816aff0034536bcc8
Kazuma Takata <neilbuzztakata@gmail.com>
2019-02-14 23:46:01 +0900

    se

---
+++ terrain/fragment_shader.glsl
@@ -0,0 +1,23 @@
+#version 410
+out vec4 FragColor;
+  
+// in vec3 ourColor;
+// in vec2 TexCoord;
+// in vec3 Normal;  
+// in vec3 FragPos; 
+
+uniform sampler2D ourTexture;
+uniform vec3 lightColor;
+uniform vec3 lightPos; 
+
+
+void main()
+{
+    // vec3 norm = normalize(Normal);
+    // vec3 lightDir = normalize(lightPos - FragPos); 
+    // float diff = max(dot(norm, lightDir), 0.0);
+    // vec3 diffuse = diff * lightColor;
+    // vec4 objectColor = texture(ourTexture, TexCoord);
+    // vec3 result = diffuse * vec3( objectColor );
+    FragColor = vec4(1.0 , 1.0, 1.0, 1.0);
+}
---
+++ terrain/main.cpp
@@ -0,0 +1,225 @@
+#include <GL/glew.h>    /* include GLEW and new version of GL on Windows */
+#include <GLFW/glfw3.h> /* GLFW helper library */
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+#ifndef NVectorC
+#define NVectorC
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#endif
+
+#include "shader.hpp"
+
+int init(GLFWwindow **window);
+void key_callback(GLFWwindow *window, int key, int scancode, int action, int mods);
+
+glm::vec3 cameraPos = glm::vec3(0.0f, 3.0f, 3.0f);
+glm::vec3 cameraFront = glm::vec3(0.0f, 0.0f, -1.0f);
+glm::vec3 cameraUp = glm::vec3(0.0f, 1.0f, 0.0f);
+
+int main()
+{
+    GLFWwindow *window = NULL;
+    GLuint vao;
+    GLuint vbo;
+
+    vector<float> vertices;
+
+    float points[] = {
+        0.0f, 0.0f, 0.0f, 0.5f, 0.0f, 0.0f, 0.5f, 0.0f, 0.5f, //
+        0.0f, 0.0f, 0.0f, 0.5f, 0.0f, 0.5f, 0.0f, 0.0f, 0.5f, //
+    };
+
+    for (int i = 0; i < 10; i++)
+    {
+        for (int i2 = 0; i2 < 10; i2++)
+        {
+
+            for (int j = 0; j < 18; j++)
+            {
+                if (j % 3 == 0)
+                {
+                    vertices.push_back(points[j] + 0.5f * i);
+                }
+                else if (j % 3 == 2)
+                {
+                    vertices.push_back(points[j] + 0.5f * i2);
+                }
+                else
+                {
+                    vertices.push_back(points[j]);
+                }
+            }
+        }
+    }
+
+    string dir = "/Users/kazumatakata/c++/opengl/terrain/";
+
+    int err = init(&window);
+    if (err == 1)
+    {
+        return 1;
+    }
+    shader myshader(dir + "vertex_shader.glsl", dir + "fragment_shader.glsl");
+    myshader.addTexture("wood.jpg");
+
+    /* a vertex buffer object (VBO) is created here. this stores an array of
+	data on the graphics adapter's memory. in our case - the vertex points */
+    glGenBuffers(1, &vbo);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(float), &vertices[0], GL_STATIC_DRAW);
+
+    /* the vertex array object (VAO) is a little descriptor that defines which
+	data from vertex buffer objects should be used as input variables to vertex
+	shaders. in our case - use our only VBO, and say 'every three floats is a
+	variable' */
+    glGenVertexArrays(1, &vao);
+    glBindVertexArray(vao);
+    /* "attribute #0 should be enabled when this vao is bound" */
+
+    /* this VBO is already bound, but it's a good habit to explicitly specify which
+	VBO's data the following vertex attribute pointer refers to */
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    /* "attribute #0 is created from every 3 variables in the above buffer, of type
+	float (i.e. make me vec3s)" */
+    // glGenBuffers(1, &EBO);
+    // glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    // glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), NULL);
+    glEnableVertexAttribArray(0);
+
+    // glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void *)(3 * sizeof(float)));
+    // glEnableVertexAttribArray(1);
+
+    // glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void *)(3 * sizeof(float)));
+    // glEnableVertexAttribArray(2);
+
+    // glVertexAttribPointer(3, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void *)(5 * sizeof(float)));
+    // glEnableVertexAttribArray(3);
+
+    glm::mat4 Projection = glm::perspective(glm::radians(45.0f), (float)4 / (float)3, 0.1f, 100.0f);
+
+    while (!glfwWindowShouldClose(window))
+    {
+
+        // float radius = 10.0f;
+        // float camX = sin(glfwGetTime()) * radius;
+        // float camZ = cos(glfwGetTime()) * radius;
+
+        // glm::mat4 View = glm::lookAt(
+        //     glm::vec3(camX, 0.0, camZ), // Camera is at (4,3,3), in World Space
+        //     glm::vec3(0, 0, 0),         // and looks at the origin
+        //     glm::vec3(0, 1, 0)          // Head is up (set to 0,-1,0 to look upside-down)
+        // );
+
+        glm::mat4 View = glm::lookAt(cameraPos, glm::vec3(0.0f, 0.0f, 0.0f), cameraUp);
+
+        /* wipe the drawing surface clear */
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        myshader.use();
+        myshader.bindTexture("ourTexture", 0);
+
+        float timeValue = glfwGetTime();
+        float greenValue = sin(timeValue) / 2.0f + 0.5f;
+
+        myshader.set4f("ourColor", 0.0f, greenValue, 0.0f, 1.0f);
+
+        glm::mat4 Model = glm::mat4(1.0f);
+        // Model = glm::rotate(Model, (float)glfwGetTime(), glm::vec3(0.0, 0.0, 1.0));
+        // Model = glm::scale(Model, glm::vec3(0.5, 0.5, 0.5));
+
+        glm::mat4 mvp = Projection * View * Model;
+
+        myshader.setMatrix4f("transform", mvp);
+        // myshader.setMatrix4f("model", Model);
+        // myshader.set3f("lightColor", 0.5f, 0.2f, 0.9f);
+        // myshader.set3f("lightPos", 5.0f, 4.0f, 1.0f);
+
+        glBindVertexArray(vao);
+        /* draw points 0-3 from the currently bound VAO with current in-use shader */
+
+        glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+        // glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        glDrawArrays(GL_TRIANGLES, 0, 3 * 200);
+
+        // glBufferData(GL_ARRAY_BUFFER, sizeof(points2), points2, GL_STATIC_DRAW);
+
+        // glBindVertexArray(vao);
+
+        // glDrawArrays(GL_TRIANGLES, 0, 3 * 2);
+
+        glfwSetKeyCallback(window, key_callback);
+
+        /* update other events like input handling */
+        glfwPollEvents();
+        /* put the stuff we've been drawing onto the display */
+        glfwSwapBuffers(window);
+    }
+
+    /* close GL context and any other GLFW resources */
+    glfwTerminate();
+    return 0;
+}
+
+void key_callback(GLFWwindow *window, int key, int scancode, int action, int mods)
+{
+    float cameraSpeed = 0.05f; // adjust accordingly
+    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+        cameraPos += cameraSpeed * cameraFront;
+    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+        cameraPos -= cameraSpeed * cameraFront;
+    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+        cameraPos -= glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
+    if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+        cameraPos += glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
+}
+
+int init(GLFWwindow **window)
+{
+
+    const GLubyte *renderer;
+    const GLubyte *version;
+    if (!glfwInit())
+    {
+        fprintf(stderr, "ERROR: could not start GLFW3\n");
+        return 1;
+    }
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    (*window) = glfwCreateWindow(640, 480, "Hello Triangle", NULL, NULL);
+    if (!(*window))
+    {
+        fprintf(stderr, "ERROR: could not open window with GLFW3\n");
+        glfwTerminate();
+        return 1;
+    }
+    glfwMakeContextCurrent((*window));
+
+    /* start GLEW extension handler */
+    glewExperimental = GL_TRUE;
+    glewInit();
+
+    /* get version info */
+    renderer = glGetString(GL_RENDERER); /* get renderer string */
+    version = glGetString(GL_VERSION);   /* version as a string */
+    printf("Renderer: %s\n", renderer);
+    printf("OpenGL version supported %s\n", version);
+
+    /* tell GL to only draw onto a pixel if the shape is closer to the viewer
+	than anything already drawn at that pixel */
+    glEnable(GL_DEPTH_TEST); /* enable depth-testing */
+    /* with LESS depth-testing interprets a smaller depth value as meaning "closer" */
+    glDepthFunc(GL_LESS);
+
+    return 0;
+}
